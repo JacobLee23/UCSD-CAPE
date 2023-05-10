d18301e04065cb4e040032e6718512be6aade847
Jacob Lee <JLpython@outlook.com>
2023-05-10 09:51:39 -0700

    feat: parse CAPE results table

--- ucsdcape/capedata.py
+++ ucsdcape/capedata.py
@@ -5,6 +5,8 @@ import json
 import pathlib
 import typing
 
+import pandas as pd
+
 
 class _CAPEData:
     """
@@ -30,9 +32,40 @@ class CAPEResults(_CAPEData):
     def __init__(self, path: typing.Union[str, pathlib.Path]):
         super().__init__(path, "results")
 
+    def __repr__(self) -> str:
+        return f"{type(self).__name__}(name='{self.name}', course_number='{self.course_number}')"
+
+    @property
+    def name(self) -> str:
+        """
+        """
+        return self.data.get("name")
+    
+    @property
+    def course_number(self) -> str:
+        """
+        """
+        return self.data.get("courseNumber")
+    
+    @property
+    def results(self) -> pd.DataFrame:
+        """
+        """
+        results = self.data.get("results")
+        return pd.DataFrame(columns=results[0], data=results[1:])
+
 
 class CAPEReport(_CAPEData):
     """
     """
     def __init__(self, path: typing.Union[str, pathlib.Path]):
         super().__init__(path, "report")
+
+    def __repr__(self) -> str:
+        return f"{type(self).__name__}(section_id={self.section_id})"
+
+    @property
+    def section_id(self) -> int:
+        """
+        """
+        return self.data.get("sectionID")
