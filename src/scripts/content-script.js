0e08c7bf929b85ff1da4160579ba0403308d50f4
Jacob Lee <JLpython@outlook.com>
2023-05-11 08:07:26 -0700

    feat: scrape SelfCAPE report study hours per week table

--- src/scripts/content-script.js
+++ src/scripts/content-script.js
@@ -444,7 +444,28 @@ class SelfCAPE {
     }
 
     scrapeStudyHoursPerWeek() {
+        const res = new Map();
+
         const eTable = document.querySelector("table:nth-child(6)");
+        const eHeaders = Array.from(eTable.querySelectorAll("tr:nth-child(1) > td")).slice(2);
+        const ePrompt = eTable.querySelector("tr:nth-child(2) > td:nth-child(2)");
+        const eData = Array.from(eTable.querySelectorAll("tr:nth-child(2) > td")).slice(2);
+        const ePercentages = Array.from(eTable.querySelectorAll("tr:nth-child(3) > td")).slice(2);
+
+        const headers = ["prompt", ...eHeaders.slice(0, -2).map((e) => e.innerText.trim()), "n", "mean"];
+        const data = [];
+
+        data.push(ePrompt.innerText.trim());
+        for (let i = 0; i < eData.length - 2; ++i) {
+            data.push(
+                {n: parseInt(eData[i].innerText.trim()), pct: ePercentages[i].innerText.trim()}
+            );
+        }
+        data.push(parseInt(eData.at(-2).innerText.trim()));
+        data.push(parseFloat(eData.at(-1).innerText.trim()));
+
+        headers.forEach((x) => { res.set(x, data[headers.indexOf(x)]); });
+        return Object.fromEntries(res.entries());
     }
 
     scrapeAttendanceFrequency() {
