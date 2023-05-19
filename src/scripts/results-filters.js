d4f84a568c8bd38c123a97d1036f29efbed8321f
Jacob Lee <JLpython@outlook.com>
2023-05-19 16:14:46 -0700

    feat: scrape CAPE results filter ranges

--- src/scripts/results-filters.js
+++ src/scripts/results-filters.js
@@ -7,7 +7,19 @@ function insertCAPEResultsFilters(message, sender, sendResponse) {
     if (message.type != "filters") { return; }
 
     const filters = new CAPEResultsFilters(message.queryParameters.Name, message.queryParameters.CourseNumber);
-    console.log(filters.instructors);
+}
+
+
+/**
+ * 
+ * @param {*} x 
+ * @param {*} n 
+ * @param {*} increment 
+ * @returns 
+ */
+function adjustToMultiple(x, n, increment = true) {
+    while (x % n != 0) { x += (increment ? 1 : -1); }
+    return x;
 }
 
 
@@ -21,7 +33,7 @@ class CAPEResultsFilters {
      * @param {*} courseNumber 
      */
     constructor(name, courseNumber) {
-        this.name = name, this.courseNumber = courseNumber;
+        this._name = name, this._courseNumber = courseNumber;
 
         this.eTable = document.getElementById("ContentPlaceHolder1_gvCAPEs");
     }
@@ -41,42 +53,80 @@ class CAPEResultsFilters {
 
     _gradeOptions() {
         const res = [];
-        Array.from("ABCDF").forEach(
-            (a) => res.push(...["+", "", "-"].map((b) => a.concat(b)))
-        );
+        Array.from("ABCDF").forEach((a) => res.push(...["+", "", "-"].map((b) => a.concat(b))));
         return res;
     }
 
-    get instructors() {
+    _gpaRange() { return [0, 4]; }
+
+    _percentageRange() { return [0, 100]; }
+
+    get instructor() {
         return this._filterOptions(
             this._scrapeTableColumn(1).map((e) => e.innerText.trim())
         );
     }
 
-    get courseNumbers() {
+    get courseNumber() {
         const re = /^(\w{3,4})\s(\d{1,3}\w{0,2})/;
         return this._filterOptions(
             this._scrapeTableColumn(2).map((e) => re.exec(e.innerText.trim())[0])
         );
     }
 
-    get quarters() {
+    get quarter() {
         const re = /^(FA|WI|SP|SU|S1|S2)(\d+)$/;
         return this._filterOptions(
             this._scrapeTableColumn(3).map((e) => re.exec(e.innerText.trim())[1])
         );
     }
 
-    get years() {
+    get year() {
         const re = /^(FA|WI|SP|SU|S1|S2)(\d+)$/;
         return this._filterOptions(
             this._scrapeTableColumn(3).map((e) => re.exec(e.innerText.trim())[2])
+        ).map((x) => 2000 + parseInt(x));
+    }
+
+    get yearRange() {
+        return [
+            adjustToMultiple(Math.min(this.year), 5, false),
+            adjustToMultiple(Math.max(this.year), 5, true)
+        ];
+    }
+
+    get enrollmentRange() {
+        let values = this._filterOptions(
+            this._scrapeTableColumn(4).map((e) => parseInt(e.innerText.trim()))
         );
+        return [0, adjustToMultiple(Math.max(values), 25)];
+    }
+
+    get evaluationsRange() {
+        let values = this._filterOptions(
+            this._scrapeTableColumn(5).map((e) => parseInt(e.innerText.trim()))
+        );
+        return [0, adjustToMultiple(Math.max(values), 25)];
+    }
+
+    get recommendClass() { return this._percentageRange(); }
+
+    get recommendInstructor() { return this._percentageInstructor(); }
+
+    get studyHoursPerWeek() {
+        let values = this._filterOptions(
+            this._scrapeTableColumn(6).map((e) => parseFloat(e.innerText.trim()))
+        );
+        return [0, adjustToMultiple(Math.ceil(Math.max(values)), 5)];
     }
 
     get averageExpectedGrade() { return this._gradeOptions(); }
 
+    get averageExpectedGradeRange() { return this._gpaRange(); }
+
     get averageReceivedGrade() { return this._gradeOptions(); }
+
+    get averageReceivedGradeRange() { return this._gpaRange(); }
 }
 
 
