2fcb1d9a98ba0d037cf0582cd771b0ccb962a50a
Jacob Lee <JLpython@outlook.com>
2023-05-19 14:53:51 -0700

    feat: scrape CAPE results filter options

--- src/scripts/results-filters.js
+++ src/scripts/results-filters.js
@@ -7,6 +7,7 @@ function insertCAPEResultsFilters(message, sender, sendResponse) {
     if (message.type != "filters") { return; }
 
     const filters = new CAPEResultsFilters(message.queryParameters.Name, message.queryParameters.CourseNumber);
+    console.log(filters.instructors);
 }
 
 
@@ -22,7 +23,7 @@ class CAPEResultsFilters {
     constructor(name, courseNumber) {
         this.name = name, this.courseNumber = courseNumber;
 
-        this.eTable = document.getElementById("ContentPlaceHolder1_gvCAPEs")
+        this.eTable = document.getElementById("ContentPlaceHolder1_gvCAPEs");
     }
 
     /**
@@ -36,16 +37,46 @@ class CAPEResultsFilters {
         );
     }
 
-    /**
-     * 
-     * @returns 
-     */
-    scrapeInstructors() {
-        const elements = this._scrapeTableColumn(1);
-        return Array.from(
-            new Set(elements.map((e) => e.innerText.trim()))
-        ).sort()
+    _filterOptions(array) { return Array.from(new Set(array)).sort(); }
+
+    _gradeOptions() {
+        const res = [];
+        Array.from("ABCDF").forEach(
+            (a) => res.push(...["+", "", "-"].map((b) => a.concat(b)))
+        );
+        return res;
     }
+
+    get instructors() {
+        return this._filterOptions(
+            this._scrapeTableColumn(1).map((e) => e.innerText.trim())
+        );
+    }
+
+    get courseNumbers() {
+        const re = /^(\w{3,4})\s(\d{1,3}\w{0,2})/;
+        return this._filterOptions(
+            this._scrapeTableColumn(2).map((e) => re.exec(e.innerText.trim())[0])
+        );
+    }
+
+    get quarters() {
+        const re = /^(FA|WI|SP|SU|S1|S2)(\d+)$/;
+        return this._filterOptions(
+            this._scrapeTableColumn(3).map((e) => re.exec(e.innerText.trim())[1])
+        );
+    }
+
+    get years() {
+        const re = /^(FA|WI|SP|SU|S1|S2)(\d+)$/;
+        return this._filterOptions(
+            this._scrapeTableColumn(3).map((e) => re.exec(e.innerText.trim())[2])
+        );
+    }
+
+    get averageExpectedGrade() { return this._gradeOptions(); }
+
+    get averageReceivedGrade() { return this._gradeOptions(); }
 }
 
 
