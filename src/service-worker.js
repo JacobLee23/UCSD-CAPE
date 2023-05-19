829660c0524dce8c532f881b2dae36c41d854252
Jacob Lee <JLpython@outlook.com>
2023-05-19 11:31:12 -0700

    refactor: src/service-worker

--- src/service-worker.js
+++ src/service-worker.js
@@ -5,61 +5,65 @@
 
 /**
  * 
- * @param {*} tab 
  */
-function scrapeCAPEPage(tab) {
-    if (!tab.url) { return; }
+class CAPEPage {
+    /**
+     * 
+     * @param {*} tab 
+     * @returns 
+     */
+    static scrape(tab) {
+        if (!tab.url) { return; }
 
-    const queryParameters = new URLSearchParams(tab.url.split("?")[1]);
+        const queryParameters = new URLSearchParams(tab.url.split("?")[1]);
 
-    let message = new Map();
-    message.set("queryParameters", Object.fromEntries(queryParameters.entries()));
-    message.set("url", tab.url);
+        const message = new Map();
+        message.set("queryParameters", Object.fromEntries(queryParameters.entries()));
+        message.set("url", tab.url);
 
-    if (tab.url.includes("Results.aspx")) { message.set("type", "CAPEResults"); }
-    else if (tab.url.includes("CAPEReport.aspx")) { message.set("type", "CAPEReport"); }
-    else if (tab.url.includes("detailedStats.asp")) { message.set("type", "SelfCAPE"); }
-    else { return; }
+        if (tab.url.includes("Results.aspx")) { message.set("type", "CAPEResults"); }
+        else if (tab.url.includes("CAPEReport.aspx")) { message.set("type", "CAPEReport"); }
+        else if (tab.url.includes("detailedStats.asp")) { message.set("type", "SelfCAPE"); }
+        else { return; }
 
-    const data = Object.fromEntries(message);
-    const response = chrome.tabs.sendMessage(tab.id, data).then(
-        (x) => { downloadCAPEData(x); }
-    );
-}
-
-
-/**
- * 
- * @param {*} payload 
- * @returns 
- */
-function downloadCAPEData(payload) {
-    if (!(payload && payload.capeType)) { return; }
-
-    const dataURL = [
-        "data:text/json;charset=utf-8",
-        encodeURIComponent(JSON.stringify(payload))
-    ].join(",");
-
-    let filename;
-    switch (payload.capeType) {
-        case "CAPEResults":
-            filename = `CAPEResults-${payload.courseNumber.split(" ").join("")}-${Date.now()}.json`;
-            break;
-        case "CAPEReport":
-            filename = `CAPEReport-${payload.sectionID}-${Date.now()}.json`;
-            break;
-        case "SelfCAPE":
-            filename = `SelfCAPE-${payload.sectionID}-${Date.now()}.json`;
-            break;
-        default:
-            return;
+        const data = Object.fromEntries(message);
+        chrome.tabs.sendMessage(tab.id, data).then(
+            (x) => { CAPEPage.download(x); }
+        );
     }
+    
+    /**
+     * 
+     * @param {*} payload 
+     */
+    static download(payload) {
+        if (!(payload && payload.capeType)) { return; }
 
-    chrome.downloads.download(
-        {filename: filename, saveAs: true, url: dataURL}
-    );
+        const dataURL = [
+            "data:text/json;charset=utf-8",
+            encodeURIComponent(JSON.stringify(payload))
+        ].join(",");
+
+        let filename;
+        switch (payload.capeType) {
+            case "CAPEResults":
+                filename = `CAPEResults-${payload.courseNumber.split(" ").join("")}-${Date.now()}.json`;
+                break;
+            case "CAPEReport":
+                filename = `CAPEReport-${payload.sectionID}-${Date.now()}.json`;
+                break;
+            case "SelfCAPE":
+                filename = `SelfCAPE-${payload.sectionID}-${Date.now()}.json`;
+                break;
+            default:
+                return;
+        }
+
+        chrome.downloads.download(
+            {filename: filename, saveAs: true, url: dataURL}
+        );
+    }
 }
 
 
-chrome.action.onClicked.addListener(scrapeCAPEPage);
+chrome.action.onClicked.addListener(CAPEPage.scrape);
